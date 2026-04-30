880a3b8202a580409e02876e7141777118d80146
robinmohammad26 <robinmohammad226@gmail.com>
2026-04-30 08:56:40 +0600

    Show All Tiles Product

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images:{
     remotePatterns: [
       {
-        hostname: 'i.pinimg.com'
+        hostname: 'i.ibb.co.com'
       }
     ]
   }
--- src/components/shared/TilesCard.jsx
+++ src/components/shared/TilesCard.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const TilesCard = async ({featureTile}) => {
@@ -5,11 +6,13 @@ const TilesCard = async ({featureTile}) => {
     return (
         <>
             <div className="card bg-base-100 shadow-sm p-6 space-y-3">
-                <figure className="">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes"
-                        className="rounded-xl" />
+                <figure className="relative aspect-square">
+                    <Image 
+                    src={featureTile.image}
+                    fill
+                    alt="Tiles images"
+                    className="object-cover rounded-md"
+                    />
                 </figure>
                 <div className="text-center">
                     <h2 className="card-title">{featureTile.title}</h2>
